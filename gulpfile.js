6a6705e32ebe10eeeced17682542dad543f14017
Елизавета <e.bagrintseva@gmail.com>
2021-02-04 19:04:56 +0300

    Исправляет все, кроме телефона

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,8 @@ gulp.task("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/js/**"
+    "source/js/**",
+    "source/img/*.ico",
     ], {
       base: "source"
     })
--- source/js/main.js
+++ source/js/main.js
@@ -6,6 +6,9 @@
   const navList = document.querySelector('.nav__list');
   const header = document.querySelector('.page__header');
 
+  const phoneInput = document.querySelector('.form__phone');
+  const form = document.querySelector('.form');
+
   document.addEventListener('DOMContentLoaded', function () {
     nav.classList.remove('nav--mobile');
     navList.classList.remove('nav__list--mobile');
@@ -18,16 +21,60 @@
         evt.preventDefault();
         menuButton.classList.remove('nav__button--closed');
         menuButton.classList.add('nav__button--opened');
-        nav.classList.add('nav--mobile');
+        nav.classList.add('nav--js');
         navList.classList.add('nav__list--mobile');
       } else {
         evt.preventDefault();
         menuButton.classList.remove('nav__button--opened');
         menuButton.classList.add('nav__button--closed');
-        nav.classList.remove('nav--mobile');
+        nav.classList.remove('nav--js');
         navList.classList.remove('nav__list--mobile');
       }
     });
   }
+
+  // const validatePhone = function (phoneNumber) {
+  //   // let regex = /^(\+7|7|8)?[\s\-]?\(?[0-9]{3}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+  //   // let regex = /^[\d]{1}\ \([\d]{2,3}\)\ [\d]{2,3}-[\d]{2,3}-[\d]{2,3}$/;
+  //   // let regex = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+  //   let regex = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
+  //   let phone = phoneNumber.match(regex);
+
+  //   if (phone) {
+  //     phoneInput.setCustomValidity('')
+  //   } else {
+  //     phoneInput.setCustomValidity('Введите номер телефона в формате +7(999)999-99-99')
+  //   }
+  //   // return regex.test(phone);
+  // }
+
+  // form.addEventListener('submit', function (evt) {
+  //   evt.preventDefault();
+  //   validatePhone();
+
+  // //   if (validatePhone(phone)) {
+  // //     phone.setCustomValidity('')
+  // //   } else {
+  // //     phone.setCustomValidity('Введите номер телефона в формате +7(999)999-99-99')
+  // //   }
+  // })
+  // const validatePhone = function () {
+  //   let regex = /^[\d]{1}\ \([\d]{2,3}\)\ [\d]{2,3}-[\d]{2,3}-[\d]{2,3}$/;
+  //   // let phone = phoneNumber.match(regex);
+
+  //   if (phoneInput.value.test(regex)) {
+  //     phoneInput.setCustomValidity('')
+  //     return true;
+  //   } else {
+  //     phoneInput.setCustomValidity('Введите номер телефона в формате +7(999)999-99-99')
+  //     return false;
+  //   }
+  // };
+
+
+  // form.addEventListener('submit', function (evt) {
+  //   evt.preventDefault();
+  //   validatePhone();
+  // });
 
 })();
