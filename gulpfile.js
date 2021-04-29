a7c5a6cde8d42d06e2a7bf2515f544490de21c0f
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-04-29 15:07:06 +0300

    Добавляет gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var svgstore = require("gulp-svgstore")
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
-var ghPages = require('gulp-gh-pages');
+var ghPages = require("gulp-gh-pages");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -97,10 +97,10 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task('deploy', function () {
-  return gulp.src('./build/**/*')
-    .pipe(ghPages());
-});
-
 gulp.task("build", gulp.series("clean", "copy", "css", "images", "webp", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
+
+gulp.task("deploy", function () {
+  return gulp.src("./build/**/*")
+    .pipe(ghPages());
+});
